dad9a404a8cfbe28ff8b06d6d2aaf822678a558f
annaVih <anna-kobzii@mail.ru>
2018-12-09 23:14:21 +0000

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import SearchBox from './SearchBox'
+
+class App extends Component {
+    render() {
+        return (
+            <>
+                <SearchBox />
+            </>
+        )
+    }
+}
+
+export default App
