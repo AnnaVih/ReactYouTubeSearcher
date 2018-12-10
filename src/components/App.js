e81540d86d1988f2042f4a5124e6f39004f0bf94
annaVih <anna-kobzii@mail.ru>
2018-12-10 00:38:53 +0000

    Make a request to youtube api by user input query

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
+import youTube from '../apis/youTube'
+
 import SearchBox from './SearchBox'
+import VideoList from './VideoList'
+
 
 class App extends Component {
+    state = {
+        videos: []
+    }
+
+    onQuerySubmit = async query => {
+        const videos = await youTube.get('/search', {
+            params: {
+                q: query
+            }
+        })
+
+        this.setState({videos: videos.data.items})
+    }
+
     render() {
         return (
-            <>
-                <SearchBox />
-            </>
+            <div className="container">
+                <SearchBox onQuerySubmit={this.onQuerySubmit}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
         )
     }
 }
